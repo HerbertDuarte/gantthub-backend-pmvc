8608693e1b4032404c18d1cd3bd1cc6bf0519a69
Herbert Duarte <herbertduarte.santos@gmail.com>
2025-01-02 17:51:08 -0300

    fix: trying fix src routes 3

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { swaggerConfig } from './infrastructure/plugins/swagger/swagger.config';
 import { bootstrapLogger } from './infrastructure/plugins/system-logs/bootstrap-logger';
 
+let app;
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  app.enableCors();
-
-  swaggerConfig(app);
-  app.enableShutdownHooks();
-  bootstrapLogger(app);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+    app.enableCors();
+    swaggerConfig(app);
+    app.enableShutdownHooks();
+    bootstrapLogger(app);
+    await app.init();
+  }
+  return app;
 }
 
-bootstrap();
+export default async (req, res) => {
+  const appInstance = await bootstrap();
+  const expressApp = appInstance.getHttpAdapter().getInstance();
+  return expressApp(req, res);
+};
