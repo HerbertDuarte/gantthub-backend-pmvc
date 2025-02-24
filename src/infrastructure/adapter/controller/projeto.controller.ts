d829ef4b1f1c810efa99ce3337631be968762347
herberttivic <herbert@tivic.com.br>
2025-02-24 00:28:30 -0300

    feat: crud projetos

--- src/infrastructure/adapter/controller/projeto.controller.ts
+++ src/infrastructure/adapter/controller/projeto.controller.ts
@@ -1,20 +1,28 @@
 import {
+  BadRequestException,
+  Body,
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
+  Patch,
+  Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { PrismaService } from '../../plugins/database/services/prisma.service';
 import { ProjetoPrisma } from '@prisma/client';
 import { PaginateResponse, PaginateUtil } from 'lib-test-herbert';
 import { PaginateProjetoDto } from '@/src/domain/application/dto/projeto/paginate-projeto.dto';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 
 @Controller('projeto')
 export class ProjetoController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll(
     @Query() props?: PaginateProjetoDto,
   ): Promise<PaginateResponse<ProjetoPrisma>> {
@@ -27,11 +35,15 @@ export class ProjetoController {
       pagina,
       itensPorPagina,
       queries: {},
+      orderBy: {
+        nome: 'asc',
+      },
     });
   }
 
   @Get(':id')
-  async findOne(@Param('id') projetoId: string) {
+  @UseGuards(JwtAuthGuard)
+  async getOne(@Param('id') projetoId: string) {
     const projeto = await this.prisma.projetoPrisma.findUnique({
       where: {
         id: projetoId,
@@ -48,5 +60,68 @@ export class ProjetoController {
       throw new NotFoundException('Projeto não encontrado');
     }
     return projeto;
+  }
+  @Get(':id/reduced')
+  @UseGuards(JwtAuthGuard)
+  async getOneReduced(@Param('id') projetoId: string) {
+    const projeto = await this.prisma.projetoPrisma.findUnique({
+      where: {
+        id: projetoId,
+      },
+    });
+    if (!projeto) {
+      throw new NotFoundException('Projeto não encontrado');
+    }
+    return projeto;
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() { nome, descricao }: ProjetoPrisma) {
+    if (!nome || nome.length < 3) {
+      throw new BadRequestException(
+        'Nome do projeto deve ter no mínimo 3 caracteres, sem caracteres especiais',
+      );
+    }
+    await this.prisma.projetoPrisma.create({
+      data: { nome, descricao },
+    });
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  async udpate(
+    @Body() { nome, descricao }: ProjetoPrisma,
+    @Param('id') projetoId: string,
+  ) {
+    if (!nome || nome.length < 3) {
+      throw new BadRequestException(
+        'Nome do projeto deve ter no mínimo 3 caracteres, sem caracteres especiais',
+      );
+    }
+    await this.prisma.projetoPrisma.update({
+      data: { nome, descricao },
+      where: {
+        id: projetoId,
+      },
+    });
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  async delete(@Param('id') projetoId: string) {
+    const projetoExists = await this.prisma.projetoPrisma.findUnique({
+      where: {
+        id: projetoId,
+      },
+    });
+    if (!projetoExists) {
+      throw new NotFoundException('Projeto não encontrado');
+    }
+    await this.prisma.projetoPrisma.delete({
+      where: {
+        id: projetoId,
+      },
+    });
   }
 }
